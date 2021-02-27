05b5b057494ea76260ae44a961e5643b9862552f
matumoto-yu <songbenyuzai@gmail.com>
2021-02-28 00:36:40 +0900

    xlsファイルの読み込み成功

--- app/src/main/java/com/example/excelapiapp/FirstFragment.kt
+++ app/src/main/java/com/example/excelapiapp/FirstFragment.kt
@@ -1,31 +1,54 @@
 package com.example.excelapiapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.excelapiapp.databinding.FragmentFirstBinding
+import org.apache.poi.ss.usermodel.CellType
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.nio.file.Paths
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        var file = resources.assets.open("words.xls")
+        val wb = WorkbookFactory.create(file)
+        val sheet = wb.getSheet("未分類")
+        Log.d("value", "first: ${sheet.firstRowNum}, last: ${sheet.lastRowNum}")
+        var result = ""
+        for (i in 0..sheet.lastRowNum) {
+            result += "${ sheet.getRow(i).getCell(0).stringCellValue } : ${ sheet.getRow(i).getCell(1).stringCellValue}\n"
         }
+        binding.textviewFirst.text = result
+
+
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/example/excelapiapp/MainActivity.kt
+++ app/src/main/java/com/example/excelapiapp/MainActivity.kt
@@ -6,12 +6,16 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.excelapiapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
     }
